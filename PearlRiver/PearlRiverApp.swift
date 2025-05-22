90864219450a5748f2db1a39d92e7b7305ee61cb
Developer <Lepiozdyx@gmail.com>
2025-05-22 16:01:40 -0300

    Initial Commit

---
+++ PearlRiver/PearlRiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PearlRiverApp.swift
+//  PearlRiver
+//
+//  Created by Alex on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PearlRiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
